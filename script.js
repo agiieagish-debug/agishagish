ca6a7cbdb4c01afb899579ee2644c9b63d9f613c
agiieagish-debug <agiieagish@gmail.com>
2026-07-24 17:06:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const navbar = document.querySelector(".navbar-section");
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 20) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+function opencv() {
+    window.open("Untitled document (1).pdf", "_blank");
+}
+
+
+
+var sidenav = document.querySelector(".sidenavbar-section")
+function shownavbar() {
+    sidenav.style.left = "0"
+}
+
+function closenavbar() {
+    sidenav.style.left = "-100%"
+}
+
